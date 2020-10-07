43ad634bfe138b3418721dd170fd8841d5ef6000
Marcella Benner <39217857+marcella-benner@users.noreply.github.com>
2020-10-06 21:23:05 -0400

    Update predict_scan.py

--- predict_scan.py
+++ predict_scan.py
@@ -43,8 +43,8 @@ classes = [d for d in os.listdir('training_data') if os.path.isdir(os.path.join(
 num_classes = len(classes)
 
 sess = tf.Session()
-saver = tf.train.import_meta_graph('model/rtlsdr-model.meta')
-saver.restore(sess, tf.train.latest_checkpoint('./'))
+saver = tf.train.import_meta_graph('rtlsdr-model.meta')
+saver.restore(sess, tf.train.latest_checkpoint('./model/'))
 
 graph = tf.get_default_graph()
 y_pred = graph.get_tensor_by_name("y_pred:0")
